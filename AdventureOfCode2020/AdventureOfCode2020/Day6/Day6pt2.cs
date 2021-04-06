5d9fc51926a1803aacee7dce856e02120118bf78
Robert Kärrbrant <robert.karrbr@gmail.com>
2021-04-06 23:03:56 +0200

    day 6 solution.

---
+++ AdventureOfCode2020/AdventureOfCode2020/Day6/Day6pt2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventureOfCode2020
+{
+    internal class Day6pt2
+    {
+        private List<GroupAnswer> groupAnswers;
+        public Day6pt2()
+        {
+            groupAnswers = new List<GroupAnswer>();
+            var currentGroup = new GroupAnswer();
+
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Git\AoC2020\AdventureOfCode2020\AdventureOfCode2020\Day6\data.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if(line == "")
+                {
+                    groupAnswers.Add(currentGroup);
+                    currentGroup = new GroupAnswer();
+                }
+                else
+                {
+                    currentGroup.addAnswer(line);
+                }
+            }
+            groupAnswers.Add(currentGroup);
+        }
+
+        internal void exec()
+        {
+            Console.WriteLine("day 6 - nr of sum common answer (pt2): " + groupAnswers.Select(a => a.currentCommonAnswers.Count).Sum());
+        }
+    }
+}
---
+++ AdventureOfCode2020/AdventureOfCode2020/Day6/GroupAnswer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventureOfCode2020
+{
+    internal class GroupAnswer
+    {
+        public HashSet<char> currentCommonAnswers { get; private set; }
+
+        public GroupAnswer(){}
+
+        internal void addAnswer(string newAnswers)
+        {
+            if(currentCommonAnswers == null)
+            {
+                currentCommonAnswers = new HashSet<char>(newAnswers.ToArray());
+            }
+            else
+            {
+                var intersect = new HashSet<char>(newAnswers.ToArray());
+                currentCommonAnswers.IntersectWith(intersect);
+            }
+        }
+    }
+}
